c992ed7c02c632eab8e42ab9ac6874c4a15c3ef7
JongHwanJeong <96283391+babyslayerr@users.noreply.github.com>
2024-08-17 00:07:29 +0900

    테스트 케이스 코드 정리

--- src/test/java/com/example/posts/PostControllerTest.java
+++ src/test/java/com/example/posts/PostControllerTest.java
@@ -8,7 +8,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
